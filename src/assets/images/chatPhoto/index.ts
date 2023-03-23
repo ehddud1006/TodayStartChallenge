6db533b808e24adc2575e2403d01d100a5bc2de3
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-03-23 20:30:50 +0900

    feat: 이미지 추가 / 이미지 multiple import

---
+++ src/assets/images/chatPhoto/index.ts
@@ -0,0 +1,13 @@
+import p0 from './p0.jpg';
+import p1 from './p1.jpg';
+import p2 from './p2.jpeg';
+import p3 from './p3.jpeg';
+import p4 from './p4.jpg';
+import p5 from './p5.jpg';
+import p6 from './p6.jpg';
+import p7 from './p7.jpeg';
+import p8 from './p8.jpg';
+import p9 from './p9.jpeg';
+import p10 from './p10.jpg';
+
+export default [p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10];
---
+++ src/assets/images/devJeans/index.ts
@@ -0,0 +1,9 @@
+import reactJeansImage from './reactJeans.jpg';
+import svelteJeansImage from './svelteJeans.jpg';
+
+const anonymous: { [key: string]: [string, string] } = {
+  Park: ['챌린저1', svelteJeansImage],
+  최범주: ['챌린저2', reactJeansImage],
+};
+
+export default anonymous;
