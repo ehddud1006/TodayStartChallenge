458adb17c80960a18500de1eabc92e35f565198e
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-03-23 20:32:07 +0900

    feat: 메인페이지 구현

--- src/@page/Main/Main.styles.tsx
+++ src/@page/Main/Main.styles.tsx
@@ -1,2 +1,220 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+
+export const Body = styled.div`
+  width: 100%;
+  height: calc(100% - 99px - 140px);
+  background-color: #c1d0db;
+
+  padding: 10px 12px;
+
+  & > * + * {
+    margin-top: 12px;
+  }
+
+  overflow-y: scroll;
+`;
+
+export const FooterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FooterTopIconContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 40px;
+
+  padding: 0 10px;
+
+  background-color: #f8f8f8;
+
+  border-bottom: 1px solid #e6e6e6;
+`;
+
+export const LeftIconContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+export const RightIconContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+export const LeftIcon = styled.img`
+  filter: invert(43%) sepia(3%) saturate(33%) hue-rotate(323deg);
+  width: 18px;
+  height: 18px;
+`;
+
+export const RigthIcon = styled.img`
+  filter: invert(43%) sepia(49%) saturate(0%) hue-rotate(301deg);
+  width: 18px;
+  height: 18px;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  padding: 0 12px;
+
+  width: 100%;
+  height: 100px;
+`;
+
+export const Button = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 14px;
+  color: #b2b2b2;
+  letter-spacing: 0.2px;
+
+  width: 80px;
+  height: 70px;
+
+  background-color: #f9f9f9;
+  border: 1px solid #ececec;
+  border-radius: 4px;
+`;
+
+export const DateNoticeWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const DateNoticeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 185px;
+  height: 25px;
+
+  background-color: #b7c5d0;
+  border-radius: 20px;
+
+  font-size: 13px;
+
+  padding-top: 2px;
+`;
+
+export const DateNotice = styled.span`
+  padding-left: 7px;
+`;
+
+export const ChatCalendarIcon = styled.img`
+  width: 15px;
+  height: 17px;
+
+  padding-bottom: 2px;
+`;
+
+export const MyTextContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+export const MyText = styled.span`
+  max-width: 75%;
+
+  margin: 5px 0;
+
+  padding: 7px 10px 5px 10px;
+
+  font-size: 13px;
+  line-height: 18px;
+  word-wrap: break-word;
+
+  background-color: #fbe54d;
+  border-radius: 3px;
+`;
+
+export const MyTextShortCotainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  max-width: 75%;
+
+  margin: 5px 0;
+
+  padding: 7px 10px 5px 10px;
+
+  font-size: 13px;
+  line-height: 18px;
+  word-wrap: break-word;
+
+  background-color: #fbe54d;
+  border-radius: 3px;
+`;
+
+export const MyTextShort = styled.span``;
+
+export const VisibleTotal = styled.span`
+  font-size: 11px;
+`;
+
+export const SendTime = styled.span`
+  align-self: flex-end;
+
+  color: #5d6469;
+  font-size: 12px;
+  padding: 0 7px 0 0;
+`;
+
+export const CounterTextContainer = styled.div`
+  display: flex;
+  gap: 5px;
+
+  width: 100%;
+`;
+
+export const CounterProfileImage = styled.img`
+  width: 40px;
+  height: 40px;
+
+  border-radius: 40%;
+`;
+
+export const CounterNameAndTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  max-width: calc(75% - 54px);
+  width: fit-content;
+`;
+export const CounterName = styled.span`
+  color: #5d6469;
+  font-size: 13px;
+`;
+
+export const CounterText = styled.span`
+  max-width: 100%;
+  width: fit-content;
+  margin: 5px 0;
+
+  padding: 7px 10px 5px 10px;
+
+  font-size: 13px;
+  line-height: 18px;
+  word-wrap: break-word;
+
+  background-color: #ffffff;
+  border-radius: 3px;
+`;
+
+export const ChatImage = styled.img`
+  width: 200px;
+  height: 200px;
+
+  margin-top: 7px;
+
+  border-radius: 3px;
+`;
--- src/@page/Main/Main.tsx
+++ src/@page/Main/Main.tsx
@@ -1,7 +1,133 @@
+import { css } from '@emotion/react';
+import { useRef } from 'react';
+
+import Header from '@/@components/Header';
+import PageTemplate from '@/@components/PageTemplate';
+import calendarImage from '@/assets/images/calendar.png';
+import callImage from '@/assets/images/call.png';
+import chatCalendarImage from '@/assets/images/chatCalendar.png';
+import chatPhoto from '@/assets/images/chatPhoto/index';
+import clipImage from '@/assets/images/clip.png';
+import anonymous from '@/assets/images/devJeans';
+import svelteJeansImage from '@/assets/images/devJeans/svelteJeans.jpg';
+import faceTimeImage from '@/assets/images/faceTime.png';
+import smileImage from '@/assets/images/smile.png';
+import streamImage from '@/assets/images/stream.png';
+import makeSection from '@/utils/makeSection';
+
+import data from './data';
 import * as Styled from './Main.styles';
 
+const WEEKDAYS = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+
 const MainPage = () => {
-  return <>da</>;
+  const photoNumber = useRef<number>(0);
+
+  const section = Object.entries(makeSection(data));
+
+  const temp = section.reduce((acc, cur) => {
+    const [year, month, date] = cur[0].split('-');
+    const inputDate = new Date(cur[0]);
+    const weekdayName = WEEKDAYS[inputDate.getDay()];
+
+    const temp = [
+      ...acc,
+      <Styled.DateNoticeWrapper key={cur[0]}>
+        <Styled.DateNoticeContainer>
+          <Styled.ChatCalendarIcon src={chatCalendarImage} alt='채팅 달력 아이콘' />
+          <Styled.DateNotice>{`${year}년 ${month}월 ${date}일 ${weekdayName}`}</Styled.DateNotice>
+        </Styled.DateNoticeContainer>
+      </Styled.DateNoticeWrapper>,
+    ];
+
+    cur[1].forEach(item => {
+      const hour = Number(item.Date.slice(11, 13));
+      const behindHour = item.Date.slice(13, 16);
+      const ampm = hour >= 12 ? '오후 ' : '오전 ';
+      const hour12 = hour >= 13 ? String(hour - 12).padStart(2, '0') : hour;
+
+      if (item.Message === '사진') {
+        temp.push(
+          item.User === '김동영' ? (
+            <Styled.MyTextContainer key={item.Date + item.User + item.Message}>
+              <Styled.SendTime>{ampm + hour12 + behindHour}</Styled.SendTime>
+              <Styled.ChatImage src={chatPhoto[photoNumber.current]} />
+            </Styled.MyTextContainer>
+          ) : (
+            <Styled.CounterTextContainer key={item.Date + item.User + item.Message}>
+              <Styled.CounterProfileImage src={anonymous[item.User][1]} alt='프로필 이미지' />
+              <Styled.CounterNameAndTextContainer>
+                <Styled.CounterName>{anonymous[item.User][0]}</Styled.CounterName>
+                <Styled.ChatImage src={chatPhoto[photoNumber.current]} />
+              </Styled.CounterNameAndTextContainer>
+              <Styled.SendTime>{ampm + hour12 + behindHour}</Styled.SendTime>
+            </Styled.CounterTextContainer>
+          )
+        );
+        photoNumber.current += 1;
+      }
+
+      if (item.Message !== '사진')
+        temp.push(
+          item.User === '김동영' ? (
+            <Styled.MyTextContainer key={item.Date + item.User + item.Message}>
+              <Styled.SendTime>{ampm + hour12 + behindHour}</Styled.SendTime>
+              {item.Message.length > 460 ? (
+                <Styled.MyTextShortCotainer>
+                  <Styled.MyTextShort>{item.Message.slice(0, 460) + '...'}</Styled.MyTextShort>
+                  <Styled.VisibleTotal>전체보기</Styled.VisibleTotal>
+                </Styled.MyTextShortCotainer>
+              ) : (
+                <Styled.MyText>{item.Message}</Styled.MyText>
+              )}
+            </Styled.MyTextContainer>
+          ) : (
+            <Styled.CounterTextContainer key={item.Date + item.User + item.Message}>
+              <Styled.CounterProfileImage src={anonymous[item.User][1]} alt='프로필 이미지' />
+              <Styled.CounterNameAndTextContainer>
+                <Styled.CounterName>{anonymous[item.User][0]}</Styled.CounterName>
+                <Styled.CounterText>{item.Message}</Styled.CounterText>
+              </Styled.CounterNameAndTextContainer>
+              <Styled.SendTime>{ampm + hour12 + behindHour}</Styled.SendTime>
+            </Styled.CounterTextContainer>
+          )
+        );
+    });
+
+    return temp;
+  }, [] as JSX.Element[]);
+
+  return (
+    <PageTemplate>
+      <Header />
+      <Styled.Body>{temp.map(item => item)}</Styled.Body>
+      <Styled.FooterContainer>
+        <Styled.FooterTopIconContainer>
+          <Styled.LeftIconContainer>
+            <Styled.LeftIcon src={smileImage} alt='웃는 아이콘' />
+            <Styled.LeftIcon src={clipImage} alt='클립 아이콘' />
+            <Styled.LeftIcon src={calendarImage} alt='달력 아이콘' />
+          </Styled.LeftIconContainer>
+
+          <Styled.RightIconContainer>
+            <Styled.RigthIcon
+              css={css`
+                width: 20px;
+                height: 20px;
+              `}
+              src={streamImage}
+              alt='라이브 아이콘'
+            />
+            <Styled.RigthIcon src={callImage} alt='전화 아이콘' />
+            <Styled.RigthIcon src={faceTimeImage} alt='화상전화 아이콘' />
+          </Styled.RightIconContainer>
+        </Styled.FooterTopIconContainer>
+        <Styled.TextContainer>
+          <Styled.Button>전송</Styled.Button>
+        </Styled.TextContainer>
+      </Styled.FooterContainer>
+    </PageTemplate>
+  );
 };
 
 export default MainPage;
